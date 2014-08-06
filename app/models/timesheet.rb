b384bebbca888e366358d3420d78ba7d9f7b5a2b
christinebeaubrun <christinebeaubrun@gmail.com>
2014-08-06 14:24:48 -0400

    Changes timesheet model

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -5,7 +5,7 @@ class Timesheet
   extend ActiveModel::Naming
 
   validates_presence_of :firstname, :lastname, length: { minimum: 2 }
-  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i,  :multiline => true
   validates_presence_of :start_time, :position, :work_week, :day_worked
   validates :hours_worked, :total_hours_worked, :shift_pay, :pay_rate, :grand_total, numericality: true
 
