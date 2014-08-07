21af68c3381adccb8ff26ed15f55353110e7b0e3
Chris Lehneis <cdlehneis@gmail.com>
2014-08-07 13:02:13 -0400

    minor changes in controller

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -17,23 +17,9 @@ class TimesheetsController < ApplicationController
           ConfirmationMailer.confirmation_email(@name, @message, email).deliver
         end
 
-        # ConfirmationMailer.confirmation_email(@name, @message, @email).deliver
         render :action => 'send_email'
-        # redirect_to root_path, success: "Email Sent, you gettin paid!"
-        # render :partial => ''
       else
         flash[:notice] = "ERRORS"
-        # redirect_to(:back) and return
-        # render :action => 'new'
       end
   end
-
-  # def send_email
-  #   @name = "#{@timesheet.firstname}, #{@timesheet.lastname}"
-  #   @message = @timesheet
-  #   @email = @timesheet.email
-
-  #   ConfirmationMailer.confirmation_email(@name, @message, @email).deliver
-  # end
-
 end
