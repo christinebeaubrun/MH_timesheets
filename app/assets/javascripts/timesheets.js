c2611e463b215f22fcc1ce2bc9882bb95ecfb740
Chris Lehneis <cdlehneis@gmail.com>
2014-08-07 17:36:35 -0400

    working on back button prevention

--- app/assets/javascripts/timesheets.js
+++ app/assets/javascripts/timesheets.js
@@ -1,16 +1,17 @@
 $(document).on('page:change', function(){
 
-  history.pushState(null, null, location.href);
-    window.onpopstate = function(event) {
-    history.go(1);
-  };
+  // prevent user from going back in app
+  // history.pushState(null, null, location.href);
+  //   window.onpopstate = function(event) {
+  //   history.go(1);
+  // };
+  // window.onbeforeunload = function() { return "YOUR WORK WILL BE LOST"; };
+
 
     var review = $('#review'),
         edit = $('#edit'),
         timesheet = $('#timesheet'),
         emailPreview = $('#email-preview');
-
-
 
     $('form#new_timesheet').on('change', 'input', function(){
       var $this = $(this),
