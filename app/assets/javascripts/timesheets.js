d7711aca7a260c4fc851d48ad8509335da24d069
April Rabkin <april.rabkin@gmail.com>
2014-08-08 16:24:47 -0400

    added total hours

--- app/assets/javascripts/timesheets.js
+++ app/assets/javascripts/timesheets.js
@@ -72,35 +72,43 @@ $(document).ready(function(){
 
   $('.container').on('keyup', '.rate', function(){
     var grandTotalElement = $('#timesheet_grand_total'),
-    subtotalArray = [],
-    subtotalCount = $('.subtotal').length;
-    grandTotalValue = 0;
+        grandTotalValue = 0;
 
     $('.subtotal').each(function(index, elem){
       var s = parseFloat($(elem).text());
       if (s){
         grandTotalValue += s;
       }
-
-      $(grandTotalElement).text(grandTotalValue);
+      grandTotalElement.text(grandTotalValue);
     });
   });
   //could refactor function underneath, which repeats function above except switching '.hours' and '.rate'
 
   $('.container').on('keyup', '.hours', function(){
     var grandTotalElement = $('#timesheet_grand_total'),
-    subtotalArray = [],
-    subtotalCount = $('.subtotal').length;
-    grandTotalValue = 0;
+        grandTotalValue = 0;
 
     $('.subtotal').each(function(index, elem){
       var s = parseFloat($(elem).text());
       if (s){
         grandTotalValue += s;
       }
-      $(grandTotalElement).text(grandTotalValue);
+      grandTotalElement.text(grandTotalValue);
     });
   }); 
 
-}); 
+  $('.container').on('keyup', '.hours', function(){
+    var totalHoursElement = $('#timesheet_total_hours'),
+        totalHoursValue = 0;
+    $('.hours').each(function(index, elem){
+      var h = parseFloat($(elem).val());
+      console.log(h);
+      if (h){
+        console.log("totalhours was called");       
+        totalHoursValue += h;
+      }
+      totalHoursElement.text(totalHoursValue);
+    });
+  });
 
+}); 
