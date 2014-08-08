4cd2740b8573ad2db7a3b4bfd77434c0c21b0f55
April Rabkin <april.rabkin@gmail.com>
2014-08-08 15:33:56 -0400

    It's calculating the javascript subtotal and total, not a textfield but a label.

--- app/assets/javascripts/timesheets.js
+++ app/assets/javascripts/timesheets.js
@@ -1,4 +1,5 @@
 
+
 $(document).on('page:change', function(){
     var review = $('#review'),
         edit = $('#edit'),
@@ -23,4 +24,83 @@ $(document).on('page:change', function(){
     });
 });
 
+
+$(document).ready(function(){
+
+  var form;
+  
+  form = [
+  '<div class="shift"><br><a>Shift:</a><br>',
+  'Date of shift: <input type="text"><br>',
+  'Start time: <input type="text"><br>',
+  'Position: <input type="text"><br>', //needs a name or an id? name...
+  'Rate per hour: <input type="text" class="rate"></br>',
+  'Hours this shift: <input type="text" class="hours"></br>',
+  'Subtotal: <label class="subtotal"></div>',
+  ];
+
+  function appendForm() {
+    $('#place_to_add_form').append(form.join("\n"));  
+  }
+
+  appendForm();
+  console.log('one appendForm was called');
+  
+  $('#add_form_button').click(function(){
+    appendForm();
+  });
+  
+
+
+  $('.container').on('keyup', '.rate', function(){    
+    var subtotalElement = $(this).closest('.shift').children('.subtotal');    
+    var subtotalNumber =  $(this).val() * $(this).closest('.shift').children('.hours').val();   
+    subtotalElement.text(subtotalNumber);
+  });
+  //could refactor function underneath, which repeats function above except switching '.hours' and '.rate'
+  $('.container').on('keyup', '.hours', function(){
+    var subtotalElement = $(this).closest('.shift').children('.subtotal');
+    var subtotalNumber =  $(this).val() * $(this).closest('.shift').children('.rate').val();
+    subtotalElement.text(subtotalNumber);
+
+    (function(){
+      console.log('subTotal calculation was called');
+    })();
+
+  });
+
+
+  $('.container').on('keyup', '.rate', function(){
+    var grandTotalElement = $('#timesheet_grand_total'),
+    subtotalArray = [],
+    subtotalCount = $('.subtotal').length;
+    grandTotalValue = 0;
+
+    $('.subtotal').each(function(index, elem){
+      var s = parseFloat($(elem).text());
+      if (s){
+        grandTotalValue += s;
+      }
+
+      $(grandTotalElement).text(grandTotalValue);
+    });
+  });
+  //could refactor function underneath, which repeats function above except switching '.hours' and '.rate'
+
+  $('.container').on('keyup', '.hours', function(){
+    var grandTotalElement = $('#timesheet_grand_total'),
+    subtotalArray = [],
+    subtotalCount = $('.subtotal').length;
+    grandTotalValue = 0;
+
+    $('.subtotal').each(function(index, elem){
+      var s = parseFloat($(elem).text());
+      if (s){
+        grandTotalValue += s;
+      }
+      $(grandTotalElement).text(grandTotalValue);
+    });
+  }); 
+
+}); 
 
