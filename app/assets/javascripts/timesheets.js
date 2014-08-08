d408b5a9a1eb0198dbea5b833f0a3fde28589215
Chris Lehneis <cdlehneis@gmail.com>
2014-08-08 14:08:44 -0400

    fixed javascript preview issue

--- app/assets/javascripts/timesheets.js
+++ app/assets/javascripts/timesheets.js
@@ -4,16 +4,13 @@ $(document).on('page:change', function(){
         timesheet = $('#timesheet'),
         emailPreview = $('#email-preview');
 
-    $('form#new_timesheet').on('change', 'input', function(){
-      var $this = $(this),
-          thisId = $this.attr('id');
-
-      $('span[role=' + thisId + ']').text($this.val());
-    });
-
-    review.click(function(event){
-      // emailPreview.toggle("slow");
+    review.on('click', function(event){
       event.preventDefault();
+      // iterate over formsheet inputs and associate the values to their divs
+      $('form#new_timesheet input').each(function(){
+        var thisId = $(this).attr('id');
+        $('span[role=' + thisId + ']').text($(this).val());
+      });
 
       timesheet.slideUp();
       emailPreview.slideDown();
