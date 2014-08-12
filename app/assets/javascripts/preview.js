e95e6a391b10f9e1dfc4aedea3140a288a2fe5dc
April Rabkin <april.rabkin@gmail.com>
2014-08-12 11:56:54 -0400

    it no longer overwrites all the shifts on the partial with the data from the last shift

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -8,28 +8,30 @@ $(document).on('page:change', function(){
 
   review.on('click', function(event){
     event.preventDefault();
-    var shiftPartial = [
+   var shiftPartial = [
     '<br />Date:  <span role="date"></span><br />',
     'Start time:  <span role="start_time"></span><br />', 
     'Hours worked:  <span role="hours"></span><br />',
     'Position:  <span role="position"></span><br />',
     'Pay rate:  <span role="rate"></span><br />',
-    'Subtotal pay:  <span role="subtotal"></span><br /><br />'
-    ];
+    'Subtotal pay:  <span role="subtotal"></span><br /><br /></div>'
+    ]; 
 
 
-      $.each($('.shift'), function(){
-        $('.append_shift').append(shiftPartial);
-        //use $(this).find n
-        // iterate over formsheet inputs
-        $('#new_timesheet input').each(function(){
-          var thisClass = $(this).attr('class');
-          $('span[role=' + thisClass + ']').text($(this).val());
+      $.each($('.shift'), function(index, shift){
+        var thisPartialDiv = "<div class='partial" + index + "'>";
+        var thisPartialDivClass = ".partial" + index; 
+
+        $('.append_shift').append(thisPartialDiv + shiftPartial.join("\n"));
+
+        ($(this).find($('#new_timesheet input'))).each(function(){
+          var thisRow = $(this).attr('class');
+          $(thisPartialDivClass).find($('span[role=' + thisRow + ']')).text($(this).val());
         });
 
-        $('#new_timesheet label').each(function(){
-          var thisClass = $(this).attr('class');
-          $('span[role=' + thisClass + ']').text($(this).text());
+        ($(this).find($('#new_timesheet label'))).each(function(){
+          var thisRow = $(this).attr('class');
+          $(thisPartialDivClass).find($('span[role=' + thisRow + ']')).text($(this).text());
         });
       });
 
