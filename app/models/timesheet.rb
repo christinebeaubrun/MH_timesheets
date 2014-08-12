8ae6a81ed1e3abfe3382617b74bc883834a44666
christinebeaubrun <christinebeaubrun@gmail.com>
2014-08-12 19:15:58 -0400

    Removed validations for shift and email sends now

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -6,9 +6,9 @@ class Timesheet
   extend ActiveModel::Naming
 
 
-  attr_accessor :firstname, :lastname, :email, :date, :start_time, :hours_worked, :position, :pay_rate, :grand_total, :subtotal, :shifts
+  attr_accessor :firstname, :lastname, :email, :date, :start_time, :hours_worked, :position, :pay_rate, :total_hours, :grand_total, :subtotal, :shifts
 
-  validates_presence_of :firstname, :lastname, :email, :date, :start_time, :hours_worked, :position, :pay_rate, :grand_total, :subtotal, :shifts
+  validates_presence_of :firstname, :lastname, :email, :shifts
 
   def initialize(attributes = {})
     attributes.each do |name, value|
