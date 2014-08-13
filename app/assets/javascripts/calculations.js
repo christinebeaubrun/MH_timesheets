8c2363f9bc2e2fba95334119b1e49f2b81217d78
April Rabkin <april.rabkin@gmail.com>
2014-08-13 18:13:28 -0400

    put hidden shift in html.erb file instead of jquery

--- app/assets/javascripts/calculations.js
+++ app/assets/javascripts/calculations.js
@@ -1,11 +1,25 @@
-$(document).on('page:change', function(){
-  // $('.shift').on('keyup', $(this), function(){
-  //   var payRate = +$('.rate').val();
-  //   var shiftHours = +$('.hours').val();
-  //   $('.subtotal').val(payRate + shiftHours);
-  // });
+//why is it adding a # to the front of stuff?
+//why is the subtotal not calculating?
 
-    $('.container').on('keyup', '.rate', function(){
+
+$(document).on('page:change', function(){
+  $('#shifts-wrapper').on('keyup', '.rate', function(){    
+    var subtotalElement = $(this).closest('.shift').children('.subtotal');    
+    var subtotalNumber =  $(this).val() * $(this).closest('.shift').children('.hours').val();   
+    subtotalElement.text(subtotalNumber);
+  });
+
+
+  //could refactor function underneath, which repeats function above except switching '.hours' and '.rate'
+  $('#shifts-wrapper').on('keyup', '.hours', function(){
+    var subtotalElement = $(this).closest('.shift').children('.subtotal');
+    var subtotalNumber =  $(this).val() * $(this).closest('.shift').children('.rate').val();
+    subtotalElement.text(subtotalNumber);
+
+
+  });
+
+    $('#shifts-wrapper').on('keyup', '.rate', function(){
     var grandTotalElement = $('#timesheet_grand_total'),
         grandTotalValue = 0;
 
@@ -19,7 +33,7 @@ $(document).on('page:change', function(){
   });
   //could refactor function underneath, which repeats function above except switching '.hours' and '.rate'
 
-  $('.container').on('keyup', '.hours', function(){
+  $('#shifts-wrapper').on('keyup', '.hours', function(){
     var grandTotalElement = $('#timesheet_grand_total'),
         grandTotalValue = 0;
 
@@ -32,12 +46,11 @@ $(document).on('page:change', function(){
     });
   });
 
-  $('.container').on('keyup', '.hours', function(){
+   $('#shifts-wrapper').on('keyup', '.hours', function(){
     var totalHoursElement = $('#timesheet_total_hours'),
         totalHoursValue = 0;
     $('.hours').each(function(index, elem){
       var h = parseFloat($(elem).val());
-      console.log(h);
       if (h){
         totalHoursValue += h;
       }
