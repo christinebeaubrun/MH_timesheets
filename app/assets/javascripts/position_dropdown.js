8fbf0ddadf72d0ba991ed65c2e7b62c60d7fee08
christinebeaubrun <christinebeaubrun@gmail.com>
2014-08-18 11:28:29 -0400

    All positions except for Other have fixed pay rates

--- app/assets/javascripts/position_dropdown.js
+++ app/assets/javascripts/position_dropdown.js
@@ -1,13 +1,24 @@
-// $(document).on('page:change', function(){
-//   var position = {
-//     "Tour Guide": 32,
-//     "Co-host": 22,
-//     "Admin": 15.84
-//   };
-
 function alertSelection(position) {
+  var mhPosition = {
+    "Tour Guide": 32,
+    "Co-host": 22,
+    "Admin": 15.84
+  };
+
   if(position === "Other") {
-    alert("You selected: " + position );
+    $("input#timesheet_pay_rate.rate").prop('disabled', false);
+  }
+  else {
+        $("input#timesheet_pay_rate.rate").prop('disabled', true);
+    if (position === "Tour Guide") {
+        $("input#timesheet_pay_rate.rate").val('' + mhPosition["Tour Guide"]);
+    }
+    if (position === "Co-host") {
+        $("input#timesheet_pay_rate.rate").val('' + mhPosition["Co-host"]);
+   }
+      else {
+        $("input#timesheet_pay_rate.rate").val('' + mhPosition["Admin"]);
+      }
   }
 }
 
