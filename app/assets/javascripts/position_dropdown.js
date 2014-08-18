977d63bacf8e1751c60df211c165d1d48f12f186
christinebeaubrun <christinebeaubrun@gmail.com>
2014-08-18 10:33:59 -0400

    Working on JS position dropdown

--- app/assets/javascripts/position_dropdown.js
+++ app/assets/javascripts/position_dropdown.js
@@ -1,18 +1,18 @@
-$( document ).ready(function() {
-  var position = {
-    "Tour Guide": 32,
-    "Co-host": 22,
-    "Admin": 15.84
-  };
+// $(document).on('page:change', function(){
+//   var position = {
+//     "Tour Guide": 32,
+//     "Co-host": 22,
+//     "Admin": 15.84
+//   };
 
-  $('#MH_Position').change(function(){
-       var positionSelected = $(this).find("option:selected").attr('value');
-        function alertSelection(position) {
-            if(position === "Other") {
-              alert("You selected: " + position );
-            }
-        }
-    alertSelection(positionSelected);
-  });
+function alertSelection(position) {
+  if(position === "Other") {
+    alert("You selected: " + position );
+  }
+}
+
+$(document).on('change', "select#MH_Position", function(event){
+  var select_element = $("select option:selected")[0].value;
+  alertSelection(select_element);
 });
 
