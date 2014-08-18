b60ae585ee619960860b4ab30574f45cd2ca4a79
christinebeaubrun <christinebeaubrun@gmail.com>
2014-08-17 21:28:33 -0400

    Adding JS for position dropdown

---
+++ app/assets/javascripts/position_dropdown.js
@@ -0,0 +1,9 @@
+$(document).on('page:change', function(){
+  var position = {
+    "Tour Guide": 32,
+    "Co-host": 22,
+    "Admin": 15.84
+  }
+
+  $('select').find(":selected").text();
+};
--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -6,18 +6,16 @@ class TimesheetsController < ApplicationController
   def create
     @timesheet = Timesheet.new(params[:timesheet])
       if @timesheet.valid?
-        @name = "#{@timesheet.firstname}, #{@timesheet.lastname}"
+        @name = "#{@timesheet.fullname}"
         @message = @timesheet
         @email = @timesheet.email
+        render :action => 'send_email'
+
         payroll = "mhmailr@gmail.com"
         recipients = [payroll, @email]
-
-        # recipients.each do |email|
+        recipients.each do |email|
           ConfirmationMailer.confirmation_email(@name, @message, @email).deliver
-        # end
-
-        render :action => 'send_email'
-        
+        end
       else
         render :new
       end
